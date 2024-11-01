ab3a9ac010da270fc578118799652ab8ceb52979
Oğuzhan Doğdu <oguzhn.dgd@gmail.com>
2024-11-02 00:22:49 +0300

    user model done

--- packages/user_repository/lib/src/entities/my_user_entity.dart
+++ packages/user_repository/lib/src/entities/my_user_entity.dart
@@ -1,1 +1,35 @@
-class MyUserEntity {}
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUserEntity(
+      {required this.id,
+      required this.email,
+      required this.name,
+      this.picture});
+
+  Map<String, Object?> toDocument() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'picture': picture,
+    };
+  }
+
+  String toString() {
+    return '''UserEntity:{
+    id:$id
+    email:$email
+    name:$name
+    picture.$picture
+    }''';
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+}
