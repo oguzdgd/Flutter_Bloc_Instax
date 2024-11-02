e43e8961605b3a5a6279ac853ae94af0231a5adb
Oğuzhan Doğdu <oguzhn.dgd@gmail.com>
2024-11-02 16:44:04 +0300

    firebase auth(signin-signup) done

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,3 +1,60 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'package:user_repository/src/user_repo.dart';
 
-class FirebaseUserRepository implements UserRepository {}
+class FirebaseUserRepository implements UserRepository {
+  FirebaseUserRepository({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  final FirebaseAuth _firebaseAuth;
+
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: myUser.email,
+        password: password,
+      );
+      myUser = myUser.copywith(id: user.user!.uid);
+
+      return myUser;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,1 +1,14 @@
-abstract class UserRepository {}
+import 'package:user_repository/src/models/models.dart';
+
+abstract class UserRepository {
+  //SIGN IN
+  Future<void> signIn(String email, String password);
+
+  //SIGN OUT
+  Future<void> logOut();
+
+  //SIGN UP
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> resetPassword(String password);
+}
